a02b0288cf67a6dca1f5a8ab056342bc7a935e1e
Carlos Martín Nieto <cmn@dwim.me>
2015-04-21 20:16:48 +0200

    push: remove own copy of callbacks
    The push object knows which remote it's associated with, and therefore
    does not need to keep its own copy of the callbacks stored in the
    remote.
    
    Remove the copy and simply access the callbacks struct within the
    remote.

--- src/push.h
+++ src/push.h
@@ -37,11 +37,6 @@ struct git_push {
 
 	/* options */
 	unsigned pb_parallelism;
-
-	git_packbuilder_progress pack_progress_cb;
-	void *pack_progress_cb_payload;
-	git_push_transfer_progress transfer_progress_cb;
-	void *transfer_progress_cb_payload;
 };
 
 /**
@@ -72,27 +67,6 @@ int git_push_new(git_push **out, git_remote *remote);
 int git_push_set_options(
 	git_push *push,
 	const git_push_options *opts);
-
-/**
- * Set the callbacks for a push
- *
- * @param push The push object
- * @param pack_progress_cb Function to call with progress information during
- * pack building. Be aware that this is called inline with pack building
- * operations, so performance may be affected.
- * @param pack_progress_cb_payload Payload for the pack progress callback.
- * @param transfer_progress_cb Function to call with progress information during
- * the upload portion of a push. Be aware that this is called inline with
- * pack building operations, so performance may be affected.
- * @param transfer_progress_cb_payload Payload for the network progress callback.
- * @return 0 or an error code
- */
-int git_push_set_callbacks(
-	git_push *push,
-	git_packbuilder_progress pack_progress_cb,
-	void *pack_progress_cb_payload,
-	git_push_transfer_progress transfer_progress_cb,
-	void *transfer_progress_cb_payload);
 
 /**
  * Add a refspec to be pushed
